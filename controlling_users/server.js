3367c202862ab93d224b3e1d9fa3ace0087ead01
Ian Tseng <ianvtseng@Ians-MacBook-Pro.local>
2017-03-14 16:24:29 -0700

    pushing

---
+++ controlling_users/server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, "./client")));
+app.use(express.static(path.join(__dirname, "./node_modules")))
+
+app.listen(8000, function() {
+    console.log("listening on port 8000")
+})
